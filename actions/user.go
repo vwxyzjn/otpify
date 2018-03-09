adf6108527f96445931820f2c74a4c760f310ea2
Costa Huang <costa.huang@outlook.com>
2018-03-09 02:26:26 -0500

    Added login feature

--- actions/user.go
+++ actions/user.go
@@ -1,8 +1,27 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gobuffalo/buffalo"
+)
+
+var signingKey = []byte("mysecretkey")
 
 // UserLogin default implementation.
 func UserLogin(c buffalo.Context) error {
-	return c.Render(200, r.HTML("user/login.html"))
+
+	claims := jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Duration(24*7) * time.Hour).Unix(),
+		Id:        "2",
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return fmt.Errorf("could not sign token, %v", err)
+	}
+	return c.Render(200, r.JSON(map[string]string{"token": tokenString}))
 }
